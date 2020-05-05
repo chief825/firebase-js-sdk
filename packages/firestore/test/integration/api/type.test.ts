chief825/firebase-js-sdk

commit 9f364b8ce1ea29be6a7f84afa1d880f1ef69ebc7

Field Value migration (#2784)

packages/firestore/src/api/user_data_writer.ts
@@ -1,0 +1,154 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as firestore from '@firebase/firestore-types';
+
+import * as api from '../protos/firestore_proto_api';
+import * as log from '../util/log';
+
+import { DocumentReference, Firestore } from './database';
+import { Blob } from './blob';
+import { GeoPoint } from './geo_point';
+import { Timestamp } from './timestamp';
+import { DatabaseId } from '../core/database_info';
+import { DocumentKey } from '../model/document_key';
+import {
+  normalizeByteString,
+  normalizeNumber,
+  normalizeTimestamp,
+  typeOrder
+} from '../model/values';
+import {
+  getLocalWriteTime,
+  getPreviousValue
+} from '../model/server_timestamps';
+import { assert, fail } from '../util/assert';
+import { forEach } from '../util/obj';
+import { TypeOrder } from '../model/field_value';
+import { ResourcePath } from '../model/path';
+import { isValidResourceName } from '../remote/serializer';
+
+export type ServerTimestampBehavior = 'estimate' | 'previous' | 'none';
+
+/**
+ * Converts Firestore's internal types to the JavaScript types that we expose
+ * to the user.
+ */
+export class UserDataWriter<T = firestore.DocumentData> {
+  constructor(
+    private readonly firestore: Firestore,
+    private readonly timestampsInSnapshots: boolean,
+    private readonly serverTimestampBehavior?: ServerTimestampBehavior,
+    private readonly converter?: firestore.FirestoreDataConverter<T>
+  ) {}
+
+  convertValue(value: api.Value): unknown {
+    switch (typeOrder(value)) {
+      case TypeOrder.NullValue:
+        return null;
+      case TypeOrder.BooleanValue:
+        return value.booleanValue!;
+      case TypeOrder.NumberValue:
+        return normalizeNumber(value.integerValue || value.doubleValue);
+      case TypeOrder.TimestampValue:
+        return this.convertTimestamp(value.timestampValue!);
+      case TypeOrder.ServerTimestampValue:
+        return this.convertServerTimestamp(value);
+      case TypeOrder.StringValue:
+        return value.stringValue!;
+      case TypeOrder.BlobValue:
+        return new Blob(normalizeByteString(value.bytesValue!));
+      case TypeOrder.RefValue:
+        return this.convertReference(value.referenceValue!);
+      case TypeOrder.GeoPointValue:
+        return new GeoPoint(
+          value.geoPointValue!.latitude!,
+          value.geoPointValue!.longitude!
+        );
+      case TypeOrder.ArrayValue:
+        return this.convertArray(value.arrayValue!);
+      case TypeOrder.ObjectValue:
+        return this.convertObject(value.mapValue!);
+      default:
+        throw fail('Invalid value type: ' + JSON.stringify(value));
+    }
+  }
+
+  private convertObject(mapValue: api.MapValue): firestore.DocumentData {
+    const result: firestore.DocumentData = {};
+    forEach(mapValue.fields || {}, (key, value) => {
+      result[key] = this.convertValue(value);
+    });
+    return result;
+  }
+
+  private convertArray(arrayValue: api.ArrayValue): unknown[] {
+    return (arrayValue.values || []).map(value => this.convertValue(value));
+  }
+
+  private convertServerTimestamp(value: api.Value): unknown {
+    switch (this.serverTimestampBehavior) {
+      case 'previous':
+        const previousValue = getPreviousValue(value);
+        if (previousValue == null) {
+          return null;
+        }
+        return this.convertValue(previousValue);
+      case 'estimate':
+        return this.convertTimestamp(getLocalWriteTime(value));
+      default:
+        return null;
+    }
+  }
+
+  private convertTimestamp(value: api.Timestamp): Timestamp | Date {
+    const normalizedValue = normalizeTimestamp(value);
+    const timestamp = new Timestamp(
+      normalizedValue.seconds,
+      normalizedValue.nanos
+    );
+    if (this.timestampsInSnapshots) {
+      return timestamp;
+    } else {
+      return timestamp.toDate();
+    }
+  }
+
+  private convertReference(name: string): DocumentReference<T> {
+    const resourcePath = ResourcePath.fromString(name);
+    assert(
+      isValidResourceName(resourcePath),
+      'ReferenceValue is not valid ' + name
+    );
+    const databaseId = new DatabaseId(resourcePath.get(1), resourcePath.get(3));
+    const key = new DocumentKey(resourcePath.popFirst(5));
+
+    if (!databaseId.isEqual(this.firestore._databaseId)) {
+      // TODO(b/64130202): Somehow support foreign references.
+      log.error(
+        `Document ${key} contains a document ` +
+          `reference within a different database (` +
+          `${databaseId.projectId}/${databaseId.database}) which is not ` +
+          `supported. It will be treated as a reference in the current ` +
+          `database (${this.firestore._databaseId.projectId}/${this.firestore._databaseId.database}) ` +
+          `instead.`
+      );
+    }
+
+    return new DocumentReference(key, this.firestore, this.converter);
+  }
+}

packages/firestore/src/api/blob.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2017 Google Inc.
+ * Copyright 2017 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,11 +23,7 @@ import {
   validateArgType,
   validateExactNumberOfArgs
 } from '../util/input_validation';
-import { primitiveComparator } from '../util/misc';
-import {
-  binaryStringFromUint8Array,
-  uint8ArrayFromBinaryString
-} from '../util/byte_string';
+import { ByteString } from '../util/byte_string';
 
 /** Helper function to assert Uint8Array is available at runtime. */
 function assertUint8ArrayAvailable(): void {
@@ -57,15 +53,13 @@ function assertBase64Available(): void {
  * using the hack above to make sure no-one outside this module can call it.
  */
 export class Blob {
-  // Prefix with underscore to signal this is a private variable in JS and
-  // prevent it showing up for autocompletion.
-  // A binary string is a string with each char as Unicode code point in the
-  // range of [0, 255], essentially simulating a byte array.
-  private _binaryString: string;
+  // Prefix with underscore to signal that we consider this not part of the
+  // public API and to prevent it from showing up for autocompletion.
+  _byteString: ByteString;
 
-  private constructor(binaryString: string) {
+  constructor(byteString: ByteString) {
     assertBase64Available();
-    this._binaryString = binaryString;
+    this._byteString = byteString;
   }
 
   static fromBase64String(base64: string): Blob {
@@ -73,8 +67,7 @@ export class Blob {
     validateArgType('Blob.fromBase64String', 'string', 1, base64);
     assertBase64Available();
     try {
-      const binaryString = PlatformSupport.getPlatform().atob(base64);
-      return new Blob(binaryString);
+      return new Blob(ByteString.fromBase64String(base64));
     } catch (e) {
       throw new FirestoreError(
         Code.INVALID_ARGUMENT,
@@ -89,21 +82,19 @@ export class Blob {
     if (!(array instanceof Uint8Array)) {
       throw invalidClassError('Blob.fromUint8Array', 'Uint8Array', 1, array);
     }
-    const binaryString = binaryStringFromUint8Array(array);
-    return new Blob(binaryString);
+    return new Blob(ByteString.fromUint8Array(array));
   }
 
   toBase64(): string {
     validateExactNumberOfArgs('Blob.toBase64', arguments, 0);
     assertBase64Available();
-    return PlatformSupport.getPlatform().btoa(this._binaryString);
+    return this._byteString.toBase64();
   }
 
   toUint8Array(): Uint8Array {
     validateExactNumberOfArgs('Blob.toUint8Array', arguments, 0);
     assertUint8ArrayAvailable();
-    const buffer = uint8ArrayFromBinaryString(this._binaryString);
-    return buffer;
+    return this._byteString.toUint8Array();
   }
 
   toString(): string {
@@ -111,20 +102,7 @@ export class Blob {
   }
 
   isEqual(other: Blob): boolean {
-    return this._binaryString === other._binaryString;
-  }
-
-  _approximateByteSize(): number {
-    // Assume UTF-16 encoding in memory (see StringValue.approximateByteSize())
-    return this._binaryString.length * 2;
-  }
-
-  /**
-   * Actually private to JS consumers of our API, so this function is prefixed
-   * with an underscore.
-   */
-  _compareTo(other: Blob): number {
-    return primitiveComparator(this._binaryString, other._binaryString);
+    return this._byteString.isEqual(other._byteString);
   }
 }
 

packages/firestore/src/api/database.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2017 Google Inc.
+ * Copyright 2017 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,10 @@
 
 import * as firestore from '@firebase/firestore-types';
 
+import * as api from '../protos/firestore_proto_api';
+
 import { FirebaseApp } from '@firebase/app-types';
-import { FirebaseService, _FirebaseApp } from '@firebase/app-types/private';
+import { _FirebaseApp, FirebaseService } from '@firebase/app-types/private';
 import { DatabaseId, DatabaseInfo } from '../core/database_info';
 import { ListenOptions } from '../core/event_manager';
 import { FirestoreClient, PersistenceSettings } from '../core/firestore_client';
@@ -38,16 +40,11 @@ import { MemoryPersistenceProvider } from '../local/memory_persistence';
 import { PersistenceProvider } from '../local/persistence';
 import { Document, MaybeDocument, NoDocument } from '../model/document';
 import { DocumentKey } from '../model/document_key';
-import {
-  ArrayValue,
-  FieldValue,
-  FieldValueOptions,
-  ObjectValue,
-  RefValue,
-  ServerTimestampValue
-} from '../model/field_value';
 import { DeleteMutation, Mutation, Precondition } from '../model/mutation';
 import { FieldPath, ResourcePath } from '../model/path';
+import { JsonProtoSerializer } from '../remote/serializer';
+import { isServerTimestamp } from '../model/server_timestamps';
+import { refValue } from '../model/values';
 import { PlatformSupport } from '../platform/platform';
 import { makeConstructorPrivate } from '../util/api';
 import { assert, fail } from '../util/assert';
@@ -98,8 +95,9 @@ import {
 import {
   DocumentKeyReference,
   fieldPathFromArgument,
-  UserDataConverter
-} from './user_data_converter';
+  UserDataReader
+} from './user_data_reader';
+import { UserDataWriter } from './user_data_writer';
 import { FirebaseAuthInternalName } from '@firebase/auth-interop-types';
 import { Provider } from '@firebase/component';
 
@@ -304,7 +302,7 @@ export class Firestore implements firestore.FirebaseFirestore, FirebaseService {
   // TODO(mikelehen): Use modularized initialization instead.
   readonly _queue = new AsyncQueue();
 
-  readonly _dataConverter: UserDataConverter;
+  readonly _dataReader: UserDataReader;
 
   // Note: We are using `MemoryPersistenceProvider` as a default
   // PersistenceProvider to ensure backwards compatibility with the format
@@ -339,7 +337,7 @@ export class Firestore implements firestore.FirebaseFirestore, FirebaseService {
 
     this._persistenceProvider = persistenceProvider;
     this._settings = new FirestoreSettings({});
-    this._dataConverter = this.createDataConverter(this._databaseId);
+    this._dataReader = this.createDataReader(this._databaseId);
   }
 
   settings(settingsLiteral: firestore.Settings): void {
@@ -538,7 +536,7 @@ export class Firestore implements firestore.FirebaseFirestore, FirebaseService {
     );
   }
 
-  private createDataConverter(databaseId: DatabaseId): UserDataConverter {
+  private createDataReader(databaseId: DatabaseId): UserDataReader {
     const preConverter = (value: unknown): unknown => {
       if (value instanceof DocumentReference) {
         const thisDb = databaseId;
@@ -556,7 +554,10 @@ export class Firestore implements firestore.FirebaseFirestore, FirebaseService {
         return value;
       }
     };
-    return new UserDataConverter(preConverter);
+    const serializer = new JsonProtoSerializer(databaseId, {
+      useProto3Json: PlatformSupport.getPlatform().useProto3Json
+    });
+    return new UserDataReader(serializer, preConverter);
   }
 
   private static databaseIdFromApp(app: FirebaseApp): DatabaseId {
@@ -763,12 +764,12 @@ export class Transaction implements firestore.Transaction {
     );
     const parsed =
       options.merge || options.mergeFields
-        ? this._firestore._dataConverter.parseMergeData(
+        ? this._firestore._dataReader.parseMergeData(
             functionName,
             convertedValue,
             options.mergeFields
           )
-        : this._firestore._dataConverter.parseSetData(
+        : this._firestore._dataReader.parseSetData(
             functionName,
             convertedValue
           );
@@ -805,7 +806,7 @@ export class Transaction implements firestore.Transaction {
         documentRef,
         this._firestore
       );
-      parsed = this._firestore._dataConverter.parseUpdateVarargs(
+      parsed = this._firestore._dataReader.parseUpdateVarargs(
         'Transaction.update',
         fieldOrUpdateData,
         value,
@@ -818,7 +819,7 @@ export class Transaction implements firestore.Transaction {
         documentRef,
         this._firestore
       );
-      parsed = this._firestore._dataConverter.parseUpdateData(
+      parsed = this._firestore._dataReader.parseUpdateData(
         'Transaction.update',
         fieldOrUpdateData
       );
@@ -866,12 +867,12 @@ export class WriteBatch implements firestore.WriteBatch {
     );
     const parsed =
       options.merge || options.mergeFields
-        ? this._firestore._dataConverter.parseMergeData(
+        ? this._firestore._dataReader.parseMergeData(
             functionName,
             convertedValue,
             options.mergeFields
           )
-        : this._firestore._dataConverter.parseSetData(
+        : this._firestore._dataReader.parseSetData(
             functionName,
             convertedValue
           );
@@ -912,7 +913,7 @@ export class WriteBatch implements firestore.WriteBatch {
         documentRef,
         this._firestore
       );
-      parsed = this._firestore._dataConverter.parseUpdateVarargs(
+      parsed = this._firestore._dataReader.parseUpdateVarargs(
         'WriteBatch.update',
         fieldOrUpdateData,
         value,
@@ -925,7 +926,7 @@ export class WriteBatch implements firestore.WriteBatch {
         documentRef,
         this._firestore
       );
-      parsed = this._firestore._dataConverter.parseUpdateData(
+      parsed = this._firestore._dataReader.parseUpdateData(
         'WriteBatch.update',
         fieldOrUpdateData
       );
@@ -1062,15 +1063,12 @@ export class DocumentReference<T = firestore.DocumentData>
     );
     const parsed =
       options.merge || options.mergeFields
-        ? this.firestore._dataConverter.parseMergeData(
+        ? this.firestore._dataReader.parseMergeData(
             functionName,
             convertedValue,
             options.mergeFields
           )
-        : this.firestore._dataConverter.parseSetData(
-            functionName,
-            convertedValue
-          );
+        : this.firestore._dataReader.parseSetData(functionName, convertedValue);
     return this._firestoreClient.write(
       parsed.toMutations(this._key, Precondition.NONE)
     );
@@ -1094,7 +1092,7 @@ export class DocumentReference<T = firestore.DocumentData>
       fieldOrUpdateData instanceof ExternalFieldPath
     ) {
       validateAtLeastNumberOfArgs('DocumentReference.update', arguments, 2);
-      parsed = this.firestore._dataConverter.parseUpdateVarargs(
+      parsed = this.firestore._dataReader.parseUpdateVarargs(
         'DocumentReference.update',
         fieldOrUpdateData,
         value,
@@ -1102,7 +1100,7 @@ export class DocumentReference<T = firestore.DocumentData>
       );
     } else {
       validateExactNumberOfArgs('DocumentReference.update', arguments, 1);
-      parsed = this.firestore._dataConverter.parseUpdateData(
+      parsed = this.firestore._dataReader.parseUpdateData(
         'DocumentReference.update',
         fieldOrUpdateData
       );
@@ -1388,13 +1386,13 @@ export class DocumentSnapshot<T = firestore.DocumentData>
         );
         return this._converter.fromFirestore(snapshot, options);
       } else {
-        return this.toJSObject(
-          this._document.data(),
-          FieldValueOptions.fromSnapshotOptions(
-            options,
-            this._firestore._areTimestampsInSnapshotsEnabled()
-          )
-        ) as T;
+        const userDataWriter = new UserDataWriter(
+          this._firestore,
+          this._firestore._areTimestampsInSnapshotsEnabled(),
+          options.serverTimestamps,
+          /* converter= */ undefined
+        );
+        return userDataWriter.convertValue(this._document.toProto()) as T;
       }
     }
   }
@@ -1410,13 +1408,13 @@ export class DocumentSnapshot<T = firestore.DocumentData>
         .data()
         .field(fieldPathFromArgument('DocumentSnapshot.get', fieldPath));
       if (value !== null) {
-        return this.toJSValue(
-          value,
-          FieldValueOptions.fromSnapshotOptions(
-            options,
-            this._firestore._areTimestampsInSnapshotsEnabled()
-          )
+        const userDataWriter = new UserDataWriter(
+          this._firestore,
+          this._firestore._areTimestampsInSnapshotsEnabled(),
+          options.serverTimestamps,
+          this._converter
         );
+        return userDataWriter.convertValue(value);
       }
     }
     return undefined;
@@ -1455,48 +1453,6 @@ export class DocumentSnapshot<T = firestore.DocumentData>
         : this._document.isEqual(other._document)) &&
       this._converter === other._converter
     );
-  }
-
-  private toJSObject(
-    data: ObjectValue,
-    options: FieldValueOptions
-  ): firestore.DocumentData {
-    const result: firestore.DocumentData = {};
-    data.forEach((key, value) => {
-      result[key] = this.toJSValue(value, options);
-    });
-    return result;
-  }
-
-  private toJSValue(value: FieldValue, options: FieldValueOptions): unknown {
-    if (value instanceof ObjectValue) {
-      return this.toJSObject(value, options);
-    } else if (value instanceof ArrayValue) {
-      return this.toJSArray(value, options);
-    } else if (value instanceof RefValue) {
-      const key = value.value(options);
-      const database = this._firestore.ensureClientConfigured().databaseId();
-      if (!value.databaseId.isEqual(database)) {
-        // TODO(b/64130202): Somehow support foreign references.
-        log.error(
-          `Document ${this._key.path} contains a document ` +
-            `reference within a different database (` +
-            `${value.databaseId.projectId}/${value.databaseId.database}) which is not ` +
-            `supported. It will be treated as a reference in the current ` +
-            `database (${database.projectId}/${database.database}) ` +
-            `instead.`
-        );
-      }
-      return new DocumentReference(key, this._firestore, this._converter);
-    } else {
-      return value.value(options);
-    }
-  }
-
-  private toJSArray(data: ArrayValue, options: FieldValueOptions): unknown[] {
-    return data.internalValue.map(value => {
-      return this.toJSValue(value, options);
-    });
   }
 }
 
@@ -1541,7 +1497,7 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
     ];
     validateStringEnum('Query.where', whereFilterOpEnums, 2, opStr);
 
-    let fieldValue: FieldValue;
+    let fieldValue: api.Value;
     const fieldPath = fieldPathFromArgument('Query.where', field);
     const operator = Operator.fromString(opStr);
     if (fieldPath.isKeyField()) {
@@ -1556,11 +1512,11 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
         );
       } else if (operator === Operator.IN) {
         this.validateDisjunctiveFilterElements(value, operator);
-        const referenceList: FieldValue[] = [];
-        for (const arrayValue of value as FieldValue[]) {
+        const referenceList: api.Value[] = [];
+        for (const arrayValue of value as api.Value[]) {
           referenceList.push(this.parseDocumentIdValue(arrayValue));
         }
-        fieldValue = new ArrayValue(referenceList);
+        fieldValue = { arrayValue: { values: referenceList } };
       } else {
         fieldValue = this.parseDocumentIdValue(value);
       }
@@ -1571,7 +1527,7 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
       ) {
         this.validateDisjunctiveFilterElements(value, operator);
       }
-      fieldValue = this.firestore._dataConverter.parseQueryValue(
+      fieldValue = this.firestore._dataReader.parseQueryValue(
         'Query.where',
         value,
         // We only allow nested arrays for IN queries.
@@ -1766,7 +1722,7 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
             `${methodName}().`
         );
       }
-      return this.boundFromDocument(methodName, snap._document!, before);
+      return this.boundFromDocument(snap._document!, before);
     } else {
       const allFields = [docOrField].concat(fields);
       return this.boundFromFields(methodName, allFields, before);
@@ -1784,12 +1740,8 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
    * of the query or if any of the fields in the order by are an uncommitted
    * server timestamp.
    */
-  private boundFromDocument(
-    methodName: string,
-    doc: Document,
-    before: boolean
-  ): Bound {
-    const components: FieldValue[] = [];
+  private boundFromDocument(doc: Document, before: boolean): Bound {
+    const components: api.Value[] = [];
 
     // Because people expect to continue/end a query at the exact document
     // provided, we need to use the implicit sort order rather than the explicit
@@ -1800,10 +1752,10 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
     // results.
     for (const orderBy of this._query.orderBy) {
       if (orderBy.field.isKeyField()) {
-        components.push(new RefValue(this.firestore._databaseId, doc.key));
+        components.push(refValue(this.firestore._databaseId, doc.key));
       } else {
         const value = doc.field(orderBy.field);
-        if (value instanceof ServerTimestampValue) {
+        if (isServerTimestamp(value)) {
           throw new FirestoreError(
             Code.INVALID_ARGUMENT,
             'Invalid query. You are trying to start or end a query using a ' +
@@ -1847,7 +1799,7 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
       );
     }
 
-    const components: FieldValue[] = [];
+    const components: api.Value[] = [];
     for (let i = 0; i < values.length; i++) {
       const rawValue = values[i];
       const orderByComponent = orderBy[i];
@@ -1881,9 +1833,9 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
           );
         }
         const key = new DocumentKey(path);
-        components.push(new RefValue(this.firestore._databaseId, key));
+        components.push(refValue(this.firestore._databaseId, key));
       } else {
-        const wrapped = this.firestore._dataConverter.parseQueryValue(
+        const wrapped = this.firestore._dataReader.parseQueryValue(
           methodName,
           rawValue
         );
@@ -2080,7 +2032,7 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
    * appropriate errors if the value is anything other than a DocumentReference
    * or String, or if the string is malformed.
    */
-  private parseDocumentIdValue(documentIdValue: unknown): RefValue {
+  private parseDocumentIdValue(documentIdValue: unknown): api.Value {
     if (typeof documentIdValue === 'string') {
       if (documentIdValue === '') {
         throw new FirestoreError(
@@ -2111,10 +2063,10 @@ export class Query<T = firestore.DocumentData> implements firestore.Query<T> {
             `but '${path}' is not because it has an odd number of segments (${path.length}).`
         );
       }
-      return new RefValue(this.firestore._databaseId, new DocumentKey(path));
+      return refValue(this.firestore._databaseId, new DocumentKey(path));
     } else if (documentIdValue instanceof DocumentReference) {
       const ref = documentIdValue as DocumentReference<T>;
-      return new RefValue(this.firestore._databaseId, ref._key);
+      return refValue(this.firestore._databaseId, ref._key);
     } else {
       throw new FirestoreError(
         Code.INVALID_ARGUMENT,

packages/firestore/src/api/field_value.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2017 Google Inc.
+ * Copyright 2017 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

commit 2c2d0ad1b5854e86db3d112be2914bea0e04191c

Fix `GeoPoint` field values with a zero coordinate failing input validation (#3018)
Fix the regression introduced in #2784: a `GeoPoint` where at least one of the coordinates is zero doesn't marshal properly. This is because zeroes happen to be serialized as `undefined` (presumably because protos normally treat zero values and the absence of a value interchangeably) which then fails the strict input validation in `GeoPoint`.

Fixes #3006.

packages/firestore/src/api/user_data_writer.ts
@@ -75,10 +75,7 @@ export class UserDataWriter<T = firestore.DocumentData> {
       case TypeOrder.RefValue:
         return this.convertReference(value.referenceValue!);
       case TypeOrder.GeoPointValue:
-        return new GeoPoint(
-          value.geoPointValue!.latitude!,
-          value.geoPointValue!.longitude!
-        );
+        return this.convertGeoPoint(value.geoPointValue!);
       case TypeOrder.ArrayValue:
         return this.convertArray(value.arrayValue!);
       case TypeOrder.ObjectValue:
@@ -94,6 +91,13 @@ export class UserDataWriter<T = firestore.DocumentData> {
       result[key] = this.convertValue(value);
     });
     return result;
+  }
+
+  private convertGeoPoint(value: api.LatLng): GeoPoint {
+    return new GeoPoint(
+      normalizeNumber(value.latitude),
+      normalizeNumber(value.longitude)
+    );
   }
 
   private convertArray(arrayValue: api.ArrayValue): unknown[] {

packages/firestore/test/integration/api/type.test.ts
@@ -62,15 +62,23 @@ apiDescribe('Firestore', (persistence: boolean) => {
   it('can read and write geo point fields', () => {
     return withTestDoc(persistence, doc => {
       return doc
-        .set({ geopoint: new GeoPoint(1.23, 4.56) })
+        .set({
+          geopoint1: new GeoPoint(1.23, 4.56),
+          geopoint2: new GeoPoint(0, 0)
+        })
         .then(() => {
           return doc.get();
         })
         .then(docSnapshot => {
-          const latLong = docSnapshot.data()!['geopoint'];
+          const latLong = docSnapshot.data()!['geopoint1'];
           expect(latLong instanceof GeoPoint).to.equal(true);
           expect(latLong.latitude).to.equal(1.23);
           expect(latLong.longitude).to.equal(4.56);
+
+          const zeroLatLong = docSnapshot.data()!['geopoint2'];
+          expect(zeroLatLong instanceof GeoPoint).to.equal(true);
+          expect(zeroLatLong.latitude).to.equal(0);
+          expect(zeroLatLong.longitude).to.equal(0);
         });
     });
   });